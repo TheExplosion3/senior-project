862f1cdbf740a1fd24a29f2ff207f24fecbada84
2023Matthew Santorsa <69405764+TheExplosion3@users.noreply.github.com>
2023-01-25 17:15:04 +0000

    more setup for the model and as well as that, more outline, and the save/load ssystem is now mostly set up, still untested though.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import Model
 import tensorflow_datasets as tfds
 # standard imports
 import matplotlib as mpl
+import matplotlib.pyplot as mplpy
 import numpy as np
 import json
 import PIL
@@ -22,29 +23,85 @@ class checkpointsave:
 
 # Variable initialization along with object
 savept = checkpointsave(None, 0, 0)
+(train_ds, val_ds, test_ds), metadata = tfds.load(
+    'tf_images',
+    split=['train[:80%]', 'train[80%:90%]', 'train[90%:]'],
+    with_info=True,
+    as_supervised=True,
+)
 
 iterator = 0
-uinput = 0
+print("Iterate for how many times? ")
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
-])
+while True:
+  try:
+    uinput = int(input())
+    break
+  except TypeError:
+      print("Invalid input. Try again.")
+  except Exception:
+      print("Unknown exception occurred.")
+
+
+if os.stat("storage.json").st_size == 0:
+  model = tf.keras.Sequential([
+      tf.keras.layers.Flatten(input_shape=(28, 28)),
+      tf.keras.layers.Dense(128, activation='relu'),
+      tf.keras.layers.Dense(64, activation='relu'),
+      tf.keras.layers.Dense(10)
+  ])
+else:
+  f.open("storage.json", "w")
+  lines = f.read()
+  if [-1] is ",":
+    f.write(lines[:-1])
+    model = tf.keras.models.load_model('model.h5')
+json.load(f)
+f.close()
+
+print("Compiling Model...")
+
+model.compile(optimizer='adam',
+loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+print("Model Compiled.")
 
 print("Current Model Statistics:")
 model.summmary()
-print("Training Beginning...")
+print(f"Training beginning, running {uinput} time(s)")
+
+image, label = next(iter(train_ds))
+get_label_name = metadata.features['label'].int2str
+_ = mplpy.imshow(image)
+_ = mplpy.title(get_label_name(label))
+
+train_ds = configure_for_performance(train_ds)
+val_ds = configure_for_performance(val_ds)
+test_ds = configure_for_performance(test_ds)
+
 while iterator != uinput:
+
+  model.fit(image, label, epochs=10)
+
   iterator+=1
-  pass
+
+  if image.next() != None:
+    image, label = next(image), next(label)
+    
+    get_label_name = metadata.features['label'].int2str
+  _ = mplpy.imshow(image)
+  _ = mplpy.title(get_label_name(label))
 # Adds the correct amount of iterations to the count, amount of times run increases by 1 as well.
 checkpointsave.runner += 1
 checkpointsave.iteration = checkpointsave.iteration + iterator
-print("Training ended.")
+print("Training ended. Creating savepoint.")
 
-# safe method, first step announces it is opening file, and saving
-print("Saving Data...")
+
+# save method, first step announces it is opening file, and saving
+print("Saving data...")
+model.save('model.h5')j
+checkpointsave.savept = "model.h5"
 f = open("storage.json", "w")
 # attempts to dump py object into a json object in the file
 try:
@@ -55,6 +112,7 @@ except Exception:
     try:
       print(f"Save failed, trying again.\nAttempt number: {i}\nException: {Exception}")
       json.dump(savept, f, indent = 6)
+      f.write(',')
       break
     except:
       print("Max number of attempts reached, exiting without save.")
