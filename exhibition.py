9ddf01d967b166ec3997ca616fe5b040cb17967f
TheExplosion3 <GOwl2515@gmail.com>
2023-01-26 16:30:51 -0500

    i forgot to save in the new file :/

---
+++ exhibition.py
@@ -0,0 +1,12 @@
+# tensorflow imports
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.models import Model
+import tensorflow_datasets as tfds
+# standard imports
+import matplotlib as mpl
+import matplotlib.pyplot as mplpy
+import json
+import os
+# file imports
+from fn import comma_addremove, configure_for_performance
